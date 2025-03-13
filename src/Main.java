12180db5689b5afbfb8eaaf32a9957e36bf2e940
Bahattin Yunus Çetin <bahattinyunuscetin@hotmail.com>
2025-03-13 15:45:43 +0300

    upt
    upt

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
 public class Main {
     public static void main(String[] args){
-        int number = 45, i;
+        int number = 2, i;
         boolean isprime = true; // Başlangıçta true olarak ayarlanmalı
+
+        if(number==1){
+            System.out.println("sayı asal değildir");
+        return;
+        }
+        if(number<1){
+            System.out.println("geçersiz sayı");
+            return;
+        }
 
         for(i = 2; i <= number / 2; i++) {
             if(number % i == 0) {
